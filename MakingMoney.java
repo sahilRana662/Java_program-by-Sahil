c9767d9511bd0b0bcb745901f3367e4803aae1e7
Sahil Rana <121264623+sahilRana662@users.noreply.github.com>
2023-02-07 19:30:07 -0800

    Add files via upload
    In this assignment, you will create a Java program called MakingMoney.  This program will compute the worth of an investment today after a given period of time. 
    
    Your program should read in the investment amount (present value) in CAD, the annual interest rate, and the number of years (as an integer) and display the future investment value using the following formula:
    
    
    FV = Future Value.
    
    PV = Present Value.
    
    r = Monthly Interest Rate
    
    n = Number of months
    
     
    
    Please show the resulting amount in both CAD and in USD using the foreign exchange rate of 1 CAD = 0.747785 USD (Jan 23, 2023 18:35 UTC).
    
    For example, if you enter the amount $1000.00, annual interest rate 3.25%, and number of years 1, the future investment value is $1032.98 CAD or $772.45 USD.
    
     
    
    Here is a sample run:
    
    
    
    Enter the investment amount in CAD, for example 120000.95: 1000.00
    
    Enter annual interest rate %, for example 8.25: 3.25
    
    Enter number of years as an integer, for example 5: 1
    
    Given an investment amount of $1000.0 CAD, an annual interest rate of 3.25%, and 1 year(s) of growth,
    
    the future value will be $1032.98 CAD or $772.45 USD.
    
     
    
    Notes:
    
    To calculate the monthly interest rate, convert the annual interest rate from percent to decimal and divide by 12.
    To truncate floating point numbers to two decimal places, you can multiply the value by 100, cast it to an integer, and then divide it by 100.00.
    Given the same input (in blue above), your output should match the sample run exactly.

---
+++ MakingMoney.java
@@ -0,0 +1,32 @@
+/* 
+ *  Sahil Rana
+ *  
+ *  Student Number = 300340146
+ *  
+ *  This  is a MakingMoney program which  compute the worth of an investment today after a given period of time!
+ *  
+ */
+import java.util.Scanner;
+public class MakingMoney{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the investment amount in CAD, for example 120000.95: ");
+        double pV = sc.nextDouble();   // pV stands for Present Value in CAD
+        System.out.println();   // to add one more line
+        System.out.print("Enter annual interest rate %, for example 8.25: ");
+        double annualInterstRate = sc.nextDouble(); 
+        System.out.println();
+        System.out.print("Enter number of years as an integer, for example 5: ");
+        int years = sc.nextInt() ;
+        System.out.println();
+        double r = (annualInterstRate/100)/12; // here r is denoted for monthly interset
+        int n = years * 12;  // n is denoted as Number of months
+        double fV1 = (float)(pV * Math.pow(1+r,n))*100; 
+        double fV2 = (int)fV1;
+        double fV =  (fV2/100);  // fV is the future Value of investment in CAD
+        double usD1 = (float)(fV * 0.747785)*100;
+        double usD2 = (int) usD1;
+        double usD = usD2 /100; // usD is the future Value of investment in USD
+        System.out.println("Given an investment amount of $" + pV + " CAD, an annual interst rate of " + annualInterstRate + "%, and " + years + " year(s) of growth, the future value will be $" + fV + " CAD or $" + usD + " USD.");
+    }
+}
